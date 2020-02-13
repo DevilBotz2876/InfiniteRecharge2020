6f085828d5d1dd7ba15eeb45eb153d843240735d
Driver Station <bhsrobotix2876-programming@gmail.com>
2020-02-12 19:40:01 -0500

    uncommented axis code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,7 +9,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-// import edu.wpi.first.wpilibj.XboxController.Axis;
+import edu.wpi.first.wpilibj.XboxController.Axis;
 import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -91,13 +91,13 @@ public class RobotContainer {
 
 
         //does not work, uses buttons b and x instead
-    // new XboxTrigger(controller, Axis.kRightTrigger.value)
-    //     .whenPressed(new ArmUp(arm))
-    //     .whenReleased(new ArmStop(arm));
+    new XboxTrigger(controller, Axis.kRightTrigger.value)
+        .whenPressed(new ArmUp(arm))
+        .whenReleased(new ArmStop(arm));
 
-    // new XboxTrigger(controller, Axis.kLeftTrigger.value)
-    //     .whenPressed(new ArmDown(arm))
-    //     .whenReleased(new ArmStop(arm));
+    new XboxTrigger(controller, Axis.kLeftTrigger.value)
+        .whenPressed(new ArmDown(arm))
+        .whenReleased(new ArmStop(arm));
 
 
     new JoystickButton(controller, Button.kY.value)
